a298f5f24698c7f955647da2339a48e0f493ee2a
ludovicab <124669159+ludovicab@users.noreply.github.com>
2023-02-07 12:48:18 +0100

    Add files via upload

---
+++ maggiore.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a ;
+    int b;
+
+     printf (" inserisci i due numeri\n");
+    scanf ("%d %d", &a,&b);
+    if (a>b)
+    {
+    printf(" a é maggiore di b \n");
+    }
+    else if (a==b)
+    {
+    printf("a é uguale a b\n");
+
+    }
+    else 
+    {printf(" a é minore di b\n ");
+
+    }
+    
+}
