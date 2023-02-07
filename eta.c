221d9f6fca15e9d8fd5a767947672ed3b10e1ebb
ludovicab <124669159+ludovicab@users.noreply.github.com>
2023-02-07 14:56:25 +0100

    Add files via upload

---
+++ eta.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("inserisci la tua eta");
+    scanf("%d",&a);
+    if (a>18)
+    {
+printf("a é maggiore di 18/n");
+    }
+    else
+     { printf (" a é minore di 18/n");
+
+    }
+}
