411a04b83c61af275c44e027d15d1e54b9f1d4bf
ludovicab <124669159+ludovicab@users.noreply.github.com>
2023-02-07 10:28:16 +0100

    Add files via upload

---
+++ ludo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int numero = 22;
+double numeropiulungo = 24356765;
+float numerodecimale = 3.6; 
+char carattere = 'c';
+
+printf( "numero: %d\n",numero);
+printf ("numero piu lungo: %f\n", numeropiulungo);
+printf ("numerodecimale: %f\n",numerodecimale);
+printf ( "carattere: %c \n",carattere);
+printf ( "carattere convertito ad intero con ascii: %d\n",carattere);
+}
---
+++ nuovo.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main ()
+{
+    int x;
+
+    printf ( "22\n");
+    scanf( "%d" , &x);
+    printf ( "x: %d\n",x);
+    return (0);
+    int x = 0;
+    int y = 0
+    printf("x era : %d\n",x);
+    printf("y era: %d\n",y);
+    printf("inserisci due numeri\n");
+    scanf("%d %d", &x &y);
+    printf ("x è diventato: ")
+}
