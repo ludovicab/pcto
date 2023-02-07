b3c92cf71b9c8ba1ff06c5e862e0e594c1d564e1
ludovicab <124669159+ludovicab@users.noreply.github.com>
2023-02-07 17:02:20 +0100

    Add files via upload

---
+++ bisestile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf (" inserisci un numero:\n");
+    scanf ("%d",&a);
+    if(a && b ) 
+    {
+        printf(" a/4");
+    }
+    else
+    {
+        printf(" non a/4");
+    }
+}
